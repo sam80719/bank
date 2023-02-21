1c41ebae548d786c436ff5a23579b17719054934
chenmingsan <sam80719@gmail.com>
2023-02-21 13:46:34 +0800

    add test: 1. account_test : createRandomAccount 2. add util for random

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"github.com/sam80719/bank/db/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createRandomAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
