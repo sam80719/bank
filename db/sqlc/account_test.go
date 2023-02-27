6ccab039b046f756a5d7b6946687cc999ea75e2d
chenmingsan <sam80719@gmail.com>
2023-02-27 18:40:18 +0800

    fix account_test: TestListAccounts 1. adjust account.sql 2. run 'make sqlc'

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -79,23 +79,21 @@ func TestDeleteAccount(t *testing.T) {
 }
 
 func TestListAccounts(t *testing.T) {
-	var lastAccount Account
 	for i := 0; i < 10; i++ {
-		lastAccount = createRandomAccount(t)
+		createRandomAccount(t)
 	}
 
 	arg := ListAccountsParams{
-		Owner:  lastAccount.Owner,
 		Limit:  5,
-		Offset: 0,
+		Offset: 5,
 	}
 
 	accounts, err := testQueries.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
-	require.NotEmpty(t, accounts)
+	require.Len(t, accounts, 5)
 
 	for _, account := range accounts {
 		require.NotEmpty(t, account)
-		require.Equal(t, lastAccount.Owner, account.Owner)
 	}
+
 }
