c71cbd789a08c9131d46c04ddb3e53d6828dc133
chenmingsan <sam80719@gmail.com>
2023-03-12 20:47:28 +0800

    add api for user login: - refactor server setting - add login user and adjust request/response struct

--- api/server.go
+++ api/server.go
@@ -30,22 +30,28 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 		store:      store,
 		tokenMaker: tokenMaker,
 	}
-	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	accountRouter := router.Group("/accounts")
-	{
-		accountRouter.POST("", server.createAccount)
-		accountRouter.GET("", server.listAccount)
-		accountRouter.GET("/:id", server.getAccount)
-	}
-	router.POST("/transfers", server.createTransfer)
-	router.POST("/users", server.createUser)
-	server.router = router
+	server.setupRouter()
 	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
+
+	server.router = router
 }
 
 // Start runs the HTTP server on a specific address.
