500342bb77979146986f27daa00fb59c81cecc53
chenmingsan <sam80719@gmail.com>
2023-03-12 15:03:32 +0800

    ini token setting when gin server work

--- api/server.go
+++ api/server.go
@@ -7,23 +7,26 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/sam80719/bank/auth"
 	db "github.com/sam80719/bank/db/sqlc"
+	"github.com/sam80719/bank/util"
 )
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
+	config     util.Config
 	store      db.Store
 	tokenMaker auth.Maker
 	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and set up routing.
-func NewServer(store db.Store) (*Server, error) {
-	tokenMaker, err := auth.NewPasetoMaker("") // add key in config later
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := auth.NewPasetoMaker(config.TokenSymmetricKey) // add key in config later
 	if err != nil {
 		return nil, fmt.Errorf("can not create token maker: %w", err)
 	}
 
 	server := &Server{
+		config:     config,
 		store:      store,
 		tokenMaker: tokenMaker,
 	}
